85be97075bd5b08a26d7aeee312c5afa25424aa9
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-11 17:26:30 +0600

    Commit test for Git Push

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
 //-------
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evz2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 
